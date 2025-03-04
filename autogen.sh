6910dea903afd8a92062a8a5934a956a968657a5
kumarvbharath <64459834+kumarvbharath@users.noreply.github.com>
2025-03-04 14:06:22 +0000

    Remove all the existing source code.
    Signed-off-by: Bharath Kumar V <kumarvbharath@yahoo.co.in>

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-autoreconf --verbose --force --install || {
-	echo 'autogen.sh failed';
-	exit 1;
-}
